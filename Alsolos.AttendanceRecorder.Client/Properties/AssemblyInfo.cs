1e80fb470a012b8a7f64e44df221773ab1f02eb6
Heiner <heiner.suter@gmx.ch>
2015-04-29 13:58:31 +0200

    Error in AssemblyInfo.

--- Alsolos.AttendanceRecorder.Client/Properties/AssemblyInfo.cs
+++ Alsolos.AttendanceRecorder.Client/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("Alsolos.AttendanceRecorder.Client")]
-[assembly: AssemblyTitle("Alsolos.AttendanceRecorder.Client")]
 [assembly: AssemblyCompany("Heiner Suter")]
 [assembly: AssemblyCopyright("Copyright © Heiner Suter 2015")]
 [assembly: AssemblyVersion("1.0.*")]
