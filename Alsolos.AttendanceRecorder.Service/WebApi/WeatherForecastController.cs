ef50129ac8eff730652d915cc82d7c7168f4ab43
Heiner <heiner.suter@zuehlke.com>
2022-03-15 10:48:24 +0100

    New service project

---
+++ Alsolos.AttendanceRecorder.Service/WebApi/WeatherForecastController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Alsolos.AttendanceRecorder.Service.WebApi
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return null;
+        }
+    }
+}
